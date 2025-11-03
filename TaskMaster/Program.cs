6388aee04fd6b188401e71a80030d9e18a798d01
sharap <toptricker8@gmail.com>
2025-11-03 18:44:06 +0300

    Register controllers and DB

--- TaskMaster/Program.cs
+++ TaskMaster/Program.cs
@@ -1,9 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using TaskMaster.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite("Data Source=taskmaster.db"));
 
 var app = builder.Build();
 
