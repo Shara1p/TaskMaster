4aeca9ae14be6153acf987166e23a9b2030e3f00
sharap <toptricker8@gmail.com>
2025-11-03 18:43:51 +0300

    Add DbContext

--- TaskMaster/Data/ApplicationDbContext.cs
+++ TaskMaster/Data/ApplicationDbContext.cs
@@ -1,6 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using TaskMaster.Models;
 namespace TaskMaster.Data;
 
-public class ApplicationDbContext
+public class ApplicationDbContext : DbContext
 {
-    
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+    public DbSet<Project> Projects { get; set; }
+    public DbSet<TaskItem> Tasks { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TaskItem>()
+            .HasOne(t => t.Project)
+            .WithMany(p => p.Tasks)
+            .OnDelete(DeleteBehavior.Cascade);  // Каскадное удаление
+    }
 }
