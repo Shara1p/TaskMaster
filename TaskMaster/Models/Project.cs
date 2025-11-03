7a9d38b73858c7036749258a7026bdaeeab71125
sharap <toptricker8@gmail.com>
2025-11-03 16:17:43 +0300

    Define Models

--- TaskMaster/Models/Project.cs
+++ TaskMaster/Models/Project.cs
@@ -2,5 +2,9 @@ namespace TaskMaster.Models;
 
 public class Project
 {
-    
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+    public required DateTime Created { get; set; }
+    public List<TaskItem>? Tasks { get; set; }
 }
--- TaskMaster/Models/TaskItem.cs
+++ TaskMaster/Models/TaskItem.cs
@@ -1,6 +1,17 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace TaskMaster.Models;
 
 public class TaskItem
 {
-    
+    public int Id { get; set; }
+    public required string Title { get; set; }
+    public required string Description { get; set; }
+    public required DateTime Created { get; set; }
+    public DateTime? DueDate { get; set; }
+
+    [ForeignKey("ProjectId")]
+    public required int ProjectId { get; set; }
+
+    public required Project Project { get; set; }
 }
